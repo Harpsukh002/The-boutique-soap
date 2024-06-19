a59ae6d511f3f414f83b1fd80452b8348e43eb7b
Harpreet Kaur <W0849797@myscc.ca>
2024-06-19 19:23:53 -0400

    Add project files.

---
+++ Data/The_boutique_soapContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using The_boutique_soap.Models;
+
+namespace The_boutique_soap.Data
+{
+    public class The_boutique_soapContext : DbContext
+    {
+        public The_boutique_soapContext (DbContextOptions<The_boutique_soapContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<The_boutique_soap.Models.Movie> Movie { get; set; } = default!;
+    }
+}
